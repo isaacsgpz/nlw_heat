fb9577a5da41475f5344d301df633ae5caf7492d
Isaac <isaacsgpz@gmail.com>
2021-10-22 21:02:07 -0300

    fix background bug

--- src/js/main.js
+++ src/js/main.js
@@ -3,44 +3,41 @@ const socialMediaLinks = {
   linkedin: 'eoisaac',
   portfolio: 'eoisaac',
   instagram: '_eoisaac',
-  twitter: '_eoisaac',
+  twitter: '_eoisaac'
 }
 
 function updateSocialMediaLinks() {
   for (let li of socialLinks.children) {
-    const socialMedia = li.getAttribute('class');
+    const socialMedia = li.getAttribute('class')
 
-    if(socialMedia=== 'linkedin') {
-      li.children[0].href = `https://${socialMedia}.com/in/${socialMediaLinks[socialMedia]}`;
+    if (socialMedia === 'linkedin')
+      li.children[0].href = `https://${socialMedia}.com/in/${socialMediaLinks[socialMedia]}`
 
-    }else if(socialMedia === 'portfolio') {
-      li.children[0].href = `https://${socialMediaLinks[socialMedia]}.github.io`;
-
-    }else {
-      li.children[0].href = `https://${socialMedia}.com/${socialMediaLinks[socialMedia]}`;
-    }
-
+    else if (socialMedia === 'portfolio')
+      li.children[0].href = `https://${socialMediaLinks[socialMedia]}.github.io`
+      
+    else
+      li.children[0].href = `https://${socialMedia}.com/${socialMediaLinks[socialMedia]}`
   }
 }
 
 function getGitHubProfileData() {
-
-  const favIcon = document.querySelector("link[rel='shortcut icon']");
-  const url = `https://api.github.com/users/${socialMediaLinks.github}`;
+  const favIcon = document.querySelector("link[rel='shortcut icon']")
+  const url = `https://api.github.com/users/${socialMediaLinks.github}`
 
   fetch(url)
     .then(response => response.json())
     .then(data => {
-      userName.textContent = data.name;
-      userBio.textContent = data.bio;
-      userLink.href = data.html_url;
-      UserImage.src = data.avatar_url;
-      userLogin.textContent = data.login;
-      
-      favIcon.href = data.avatar_url;
-      document.title = `${data.name} | NLW Heat 2021`;
-    });
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      UserImage.src = data.avatar_url
+      userLogin.textContent = data.login
+
+      favIcon.href = data.avatar_url
+      document.title = `${data.name} | NLW Heat 2021`
+    })
 }
 
-updateSocialMediaLinks();
-getGitHubProfileData();
+updateSocialMediaLinks()
+getGitHubProfileData()
